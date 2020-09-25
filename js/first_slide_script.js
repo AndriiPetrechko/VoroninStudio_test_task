e8ba7a112d9c8cbb06f95f8b36febd137127dbf9
AndriiPetrechko <petrechkoandrij@gmail.com>
2020-09-25 10:34:42 +0300

    add burger menu

--- js/first_slide_script.js
+++ js/first_slide_script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $('.burger_btn').click(function(){
-        $('.collapse_nav').toggleClass('collapse_nav_active');
+    // collapse menu rotate
+    $('.burger_menu').click(function(){
+        $(this).toggleClass('burger_active');
+        $('.collapse_menu').toggleClass('collapse_menu_active');
+    })
+
+    $(".menu__item a").on("click", function() {
+        $(".menu__item a").removeClass("active");
+        $(this).addClass("active");
+      });
+
+    $(".collapse_menu_item").click(function(){
+        $('.collapse_menu').removeClass('collapse_menu_active');
+        $('.burger_menu').removeClass('burger_active');
     })
 })
